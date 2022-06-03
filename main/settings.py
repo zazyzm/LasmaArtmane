61f9c3dc282e8f42d0d7002d4eba31db7e54723d
Zane Mertes <zane_mertes@outlook.de>
2022-06-03 19:08:13 +0200

    secret key fix

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o--zw@__bwio6liq%scq4v+s_ypwq8l_=x#2je%!&_v*fd93_9'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

a7d2e75bd086ef0ed369af095511aaffa69e6443
Zane Mertes <zane_mertes@outlook.de>
2022-06-30 11:27:58 +0200

    Guard clause for setion dataframe

--- helpers/navbar/context.py
+++ helpers/navbar/context.py
@@ -8,16 +8,19 @@ def navigation_sections():
     active_sections = Section.objects.values('name').filter(is_active=True).order_by('id')
     sections = [i['name'] for i in active_sections]
     active_galeries = Gallery.objects.values('name', 'section__name').filter(is_active=True).order_by('id')
-    df = pandas.DataFrame(active_galeries)
-    df = df.groupby('section__name')['name'].agg(','.join).to_json()
-    res = json.loads(df)
-    result = {k: v.split(',') for k, v in res.items()}
-    per_section = {}
-    for section in sections:
-        for section_name, gallery in result.items():
-            if section_name == section:
-                per_section.update({section: gallery})
-                break
-            else:
-                per_section.update({section: []})
+    if active_galeries:
+        df = pandas.DataFrame(active_galeries)
+        df = df.groupby('section__name')['name'].agg(','.join).to_json()
+        res = json.loads(df)
+        result = {k: v.split(',') for k, v in res.items()}
+        per_section = {}
+        for section in sections:
+            for section_name, gallery in result.items():
+                if section_name == section:
+                    per_section.update({section: gallery})
+                    break
+                else:
+                    per_section.update({section: []})
+    else:
+        per_section = []
     return per_section

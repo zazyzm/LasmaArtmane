ec06920114685bd1827592f6ac6df1e6da2ee7e1
Zane Mertes <zane_mertes@outlook.de>
2022-06-30 09:49:42 +0200

    Added local settings import

--- main/settings.py
+++ main/settings.py
@@ -132,3 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+

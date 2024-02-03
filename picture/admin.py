d7377e951c8fd1a345fa76f3e8365f416cb942d0
Zane Mertes <zane_mertes@outlook.de>
2024-02-03 21:07:25 +0100

    Add picture example in admin

--- picture/admin.py
+++ picture/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Picture
-
+from django.utils.html import format_html
 
 class PictureAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'gallery', 'is_active')
+    def image_tag(self, obj):
+        return format_html('<img width="100" src="{}" />'.format(obj.picture.url))
+
+    image_tag.short_description = 'Image'
+    list_display = ('id', 'name', 'gallery', 'is_active', 'image_tag', 'date')
     list_filter = ('name', 'gallery', 'is_active',)
     list_editable = ('is_active',)
     search_fields = ('name', 'gallery', 'is_active')
     list_per_page = 10
-
 
 admin.site.register(Picture, PictureAdmin)
